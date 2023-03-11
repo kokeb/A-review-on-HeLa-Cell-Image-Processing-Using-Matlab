054547d1c0c175c72852e1dc86415df11f1878ae
Kokeb Dese <kokebdese86@gmail.com>
2023-03-11 15:44:38 +0000

    I have changed line 3

--- myfirstHCprocessing.m
+++ myfirstHCprocessing.m
@@ -1,5 +1,5 @@
 close all,
 clear all,
-%cd('D:\City University of London\From supervisor task\Hela cell processing\Cell_processing')
+cd('D:\City University of London\From supervisor task\Hela cell processing\Cell_processing')
 k=dmread(Prefix_3VBSED_roi_00_slice_0174.dm4);
 imagesc(k);
